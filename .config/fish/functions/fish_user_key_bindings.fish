fe0f6ee39669177af905b99b0dc8f37f1052836c
nonoshita4 <nonoshita4@gmail.com>
2021-11-28 23:45:39 +0900

    revert ctrl + l to clear terminal screen

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -4,7 +4,7 @@ function fish_user_key_bindings
   bind \cf peco_change_directory # Bind for peco change directory to Ctrl+F
 
   # vim-like
-  bind \cl forward-char
+  # bind \cl forward-char
 
   # prevent iterm2 from closing when typing Ctrl-D (EOF)
   bind \cd delete-char
